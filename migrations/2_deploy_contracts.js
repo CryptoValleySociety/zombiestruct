7bfcd796ee71615cb487a9a1afad88f49a8f2723
simonmogli <simon@scheynet.de>
2018-05-08 23:49:29 +0200

    Wrote Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ZombieHelper=artifacts.require("ZombieHelper");
+var ZombieFeeding=artifacts.require("ZombieFeeding");
+var ZombieFactory=artifacts.require("ZombieFactory");
+var Ownable=artifacts.require("Ownable");
+var SafeMath=artifacts.require("SafeMath");
 
-// module.exports = function(deployer) {
-//   deployer.deploy(ConvertLib);
-//   deployer.link(ConvertLib, MetaCoin);
-//   deployer.deploy(MetaCoin);
-// };
+module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, [ZombieHelper,ZombieFeeding,ZombieFactory,Ownable]);
+  deployer.deploy(Ownable);
+  deployer.deploy(ZombieFactory);
+  deployer.deploy(ZombieFeeding);
+  deployer.deploy(ZombieHelper);
+};
