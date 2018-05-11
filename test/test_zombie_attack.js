029c0e0b29acbc19d4d5a3fc60ad826f3053357c
Alessandro Latif <alessandro@latif-khan.com>
2018-05-11 19:11:14 +0200

    Edit var to const and let

--- test/test_zombie_attack.js
+++ test/test_zombie_attack.js
@@ -1,9 +1,9 @@
 var ZombieAttack = artifacts.require("./zombieattack.sol");
 
 contract('ZombieAttack', (accounts) => {
+    const account_one = accounts[0];
+    const account_two = accounts[1];
     let contract;
-    var account_one = accounts[0];
-    var account_two = accounts[1];
     let zombie_one;
     let zombie_two;
 
@@ -15,25 +15,25 @@ contract('ZombieAttack', (accounts) => {
         await contract.createRandomZombie("Banter", { from: account_one });
         await contract.createRandomZombie("Joker", { from: account_two });
     
-        var zombies1 = await contract.getZombiesByOwner.call(account_one);
+        const zombies1 = await contract.getZombiesByOwner.call(account_one);
         zombie_one = zombies1[0].toNumber();
-        var zombies2 = await contract.getZombiesByOwner.call(account_two);
+        const zombies2 = await contract.getZombiesByOwner.call(account_two);
         zombie_two = zombies2[0].toNumber();
 
         assert.equal(zombie_one, 0, "Zombie with ID 0 not owned by first account");
         assert.equal(zombie_two, 1, "Zombie with ID 1 not owned by second account");
     }
 
-    it("returns the modulo of the hash of msg.sender and randnonce", async () => {
-        var result = await contract.randMod.call(5, { from: account_one });
+    it("returns the modulo hash of msg.sender and randnonce", async () => {
+        const result = await contract.randMod.call(5, { from: account_one });
         assert.equal(typeof(result.toNumber()), 'number', 'return value should be zero');
     });
 
     it('creates two random zombies and attack with either a win or loss count', async () => {
         createRandomZombies()
         .then(async () => {
-            var winCount = await contract.getWinCount.call(zombie_one)
-            var lossCount = await contract.getWinCount.call(zombie_one)
+            const winCount = await contract.getWinCount.call(zombie_one)
+            const lossCount = await contract.getWinCount.call(zombie_one)
             assert.equal(winCount.toNumber(), 0, 'zombie should have no wins before attack')
             assert.equal(lossCount.toNumber(), 0, 'zombie should have no wins before attack')
 
