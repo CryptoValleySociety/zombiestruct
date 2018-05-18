5433a8d6fa5ade6b95bc0c77dee0b781fcff6985
simonmogli <simon@scheynet.de>
2018-05-18 17:43:52 +0200

    New deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,8 @@
-var ZombieHelper=artifacts.require("ZombieHelper");
-var ZombieFeeding=artifacts.require("ZombieFeeding");
-var ZombieFactory=artifacts.require("ZombieFactory");
-var Ownable=artifacts.require("Ownable");
-var SafeMath=artifacts.require("SafeMath");
+const  ZombieAttack=artifacts.require("ZombieAttack");
+const SafeMath=artifacts.require("SafeMath");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
-  deployer.link(SafeMath, [ZombieHelper,ZombieFeeding,ZombieFactory,Ownable]);
-  deployer.deploy(Ownable);
-  deployer.deploy(ZombieFactory);
-  deployer.deploy(ZombieFeeding);
-  deployer.deploy(ZombieHelper);
+  deployer.link(SafeMath, [ZombieAttack]);
+  deployer.deploy(ZombieAttack);
 };
