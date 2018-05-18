dd2268e2590b98141348a568d052d36e2835b74f
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-05-18 10:08:31 -0500

    zombieFeeding

--- test/test_zombie_feeding.js
+++ test/test_zombie_feeding.js
@@ -4,7 +4,7 @@ const ZombieHelper = artifacts.require('./zombieHelper.sol')
 
 contract('ZombieFeeding', (accounts) => {
   const accountOne = accounts[0]
-  const kittyContractAddress = '0x06012c8cf97BEaD5deAe237070F9587f8E7A266d'
+  const kittyContractAddress = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"
   let contract
   let helperContract
   let newZombie
@@ -14,7 +14,7 @@ contract('ZombieFeeding', (accounts) => {
   beforeEach(async ()  => {
         // returns instance of deployed contract
         contract = await ZombieFeeding.deployed()
-        helperContract = await ZombieHelper.deployed()
+        // helperContract = await ZombieHelper.deployed()
     });
 
     const zombieByOwner = async (owner) => {
@@ -25,17 +25,17 @@ contract('ZombieFeeding', (accounts) => {
     // first need to create a Zombie
     const createZombie = async () => {
       newZombie = await contract.createRandomZombie('Mohammad', { from: accountOne })
-      // HOW AM I GETTING A CONTRACT HERE I AM SO CONFUSED
       console.log(newZombie);
     }
 
 
     it("setKittyContractAddress test", async () => {
-        await createZombie()
+        // await createZombie()
         // if I get a DNA then the contract is set
-        // await contract.setKittyContractAddress(kittyContractAddress, { from: accountOne })
-        // const kittyDna = contract.getKitty(757681)
-        //assert.notEqual(kittyDna, null)
+        await contract.setKittyContractAddress(kittyContractAddress)
+        const kittyDna = await contract.getKitty(1)
+        // console.log(kittyDna.toNumber());
+        // assert.notEqual(kittyDna, null)
     })
 
     it("feedOnKitty test", async () => {
