545700c5fdf318bfc28aaa1ed4a057c6548c87a0
Sebastian Henningsen <sebastian.henningsen@hu-berlin.de>
2018-05-24 11:30:58 +0200

    Edit private function to be internal in zombiefactory

--- contracts/zombieFactory.sol
+++ contracts/zombieFactory.sol
@@ -34,7 +34,7 @@ contract ZombieFactory is Ownable {
     NewZombie(id, _name, _dna);
   }
 
-  function _generateRandomDna(string _str) private view returns (uint) {
+  function _generateRandomDna(string _str) internal view returns (uint) {
     uint rand = uint(keccak256(_str));
     return rand % dnaModulus;
   }
