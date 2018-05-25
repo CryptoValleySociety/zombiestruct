b8cbc86ea05c9b405638c125c210a526c774f715
Alessandro Latif <alessandro@latif-khan.com>
2018-05-25 18:50:18 +0200

    Add better component boilerplate

--- src/components/simon.js
+++ src/components/simon.js
@@ -3,23 +3,38 @@ import React, { Component } from 'react'
 import '../App.css'
 
 class Simon extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: 'this is my data as a react state'
+        }
+    }
+
     componentDidMount() {
         // initiate contract
-        // call function retriev data
+        // call function retrieve data
         console.log('component mounted')
+    }
+
+    updateData() {
+        //call the blockchain to revtrieve my data
+        var newData = "this is a change"
+        this.setState({ data: newData })
     }
 
     callFunction() {
         console.log('hello')
         // call function 
         // wait for reciept then call retrieve data function
+        this.updateData();
     }
 
     render() {
         return (
             <div className="segment" id="simon">
                 <h1>Simon</h1>
-                <p id="data">This is my data</p>
+                <p id="data">{this.state.data}</p>
                 <button id="button" onClick={() => { this.callFunction() }}></button>
             </div>
         );
