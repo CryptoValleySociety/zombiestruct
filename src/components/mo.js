d168e5de901266959f19536584031399782bc905
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-01 08:34:51 -0700

    ADD create zombie, event listener, still in progress

--- src/components/mo.js
+++ src/components/mo.js
@@ -1,47 +1,24 @@
-import React, {Component} from 'react'
-import web3 from '../utils/web3/providers/index'
-import ZombieAttack from '../../truffle/build/contracts/ZombieAttack.json'
+import React, { Component } from 'react'
 import contractMethods from '../utils/calls/component'
+
 
 import '../App.css'
 
 class Mo extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
       data: 'this is my data as a react state',
-      contract: null,
-      account: null
+      contract: this.props.contract,
+      account: this.props.accounts[4]
     }
   }
 
-  componentDidMount() {
-    this.initialize()
-  }
 
-  async initialize() {
-    const data = await contractMethods.initialize()
-    const {contract, accounts} = data
-    const account = accounts[0]
-    await this.setState({contract: contract, account: account})
+  async componentDidMount() {
+    const { contract, account } = this.state
     await this.getZombiesByOwner(contract, account)
     await this.connectToKitty()
-  }
-
-  async getZombiesByOwner(contract, account) {
-    const zombieId = await contractMethods.getZombiesByOwner(contract, account)
-    const zombie = await contractMethods.getZombieById(contract, zombieId[0])
-    if(zombie) {
-      this.setState({
-        data: zombie.name + zombie.dna
-      })
-    }
-  }
-
-  async createZombie() {
-    const { contract, account } = this.state
-    await contractMethods.createRandomZombie(contract, "Mohammad", account, 3000000)
   }
 
   async connectToKitty() {
@@ -49,9 +26,29 @@ class Mo extends Component {
     await contractMethods.connectToKitty(contract, account, 3000000)
   }
 
+  async getZombiesByOwner(contract, account) {
+    const zombieId = await contractMethods.getZombiesByOwner(contract, account)
+    // TODO: if zombie ID does not exist at the start, set data state as NO ZOMBIE CREATED
+    const zombie = await contractMethods.getZombieById(contract, zombieId[0])
+    if (zombie) {
+      this.setState({
+        data: zombie.name + ' ' + zombie.dna
+      })
+    }
+  }
+
+  async createZombie() {
+    const { contract, account } = this.state
+    // TODO: if statement if already created or VM error occurs
+    // with notification saying zombie already created
+    await contractMethods.createRandomZombie(contract, "Mohammad", account, 3000000)
+    // TODO: on creation call getZombiesByOwner to update state
+  }
+
   async feedOnKitty() {
     const { contract, account } = this.state
     await contractMethods.feedOnKitty(contract, account, 3000000)
+    // TODO: on creation call getZombiesByOwner to update state
   }
 
   render() {
