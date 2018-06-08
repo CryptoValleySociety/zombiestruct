5dcdb23aaa1d2672db8cfc92bc8f2998e5a48d40
simonmogli <simon@scheynet.de>
2018-06-08 09:40:58 +0200

    Add simon component function calls

--- src/components/simon.js
+++ src/components/simon.js
@@ -1,33 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import funcLib from '../utils/calls/funclib';
+
+//import Web3 from "web3";
+//import { contracts } from '../utils/web3/addresses/contracts';
+//import ZombieAttackAbi from '../../truffle/build/contracts/ZombieAttack.json';
 
 import '../App.css'
 
+
 class Simon extends Component {
+
     constructor(props) {
         super(props)
 
         this.state = {
-            data: 'this is my data as a react state'
+            data: 'this is my data as a react state',
+            contract: null,
+            accounts: null,
+            zombieId: -1
         }
     }
 
     componentDidMount() {
-        // initiate contract
-        // call function retrieve data
-        console.log('component mounted')
+      this.initContract();
     }
 
-    updateData() {
-        //call the blockchain to revtrieve my data
-        var newData = "this is a change"
-        this.setState({ data: newData })
+    async initContract() {
+      const initObj = await contractMethods.initialize();
+      this.setState({
+        contract: initObj.contract,
+        accounts: initObj.accounts
+      });
+
+      this.setListener()
+      this.checkZombie()
     }
 
-    callFunction() {
-        console.log('hello')
-        // call function 
-        // wait for reciept then call retrieve data function
-        this.updateData();
+    setListener() {
+      contractMethods.setNewZombieListener(this.state.contract, async (event) => {
+        this.setState({ zombieId: event.returnValues.zombieId })
+      });
+    }
+
+    async checkZombie() {
+      const zombiesInAccount = await contractMethods.getZombiesByOwner(this.state.contract, this.state.accounts[0]);
+      if (zombiesInAccount.length === 0) {
+        await contractMethods.createRandomZombie(this.state.contract, 'Simon', this.state.accounts[0], 300000);
+      }
+      await this.showZombie();
+    }
+
+    async showZombie() {
+      let zId
+      if (this.state.zombieId === -1) {
+        const ids = await contractMethods.getZombiesByOwner(this.state.contract, this.state.accounts[0]);
+        if (ids.length > 0) {
+          zId = ids[0];
+          this.setState({ zombieId: zId });
+        } else {
+          return;
+        }
+      } else {
+        zId = this.state.zombieId;
+      }
+      const zombie = await contractMethods.getZombieById(this.state.contract, zId);
+      const zName = zombie.name;
+      const zLevel = zombie.level;
+      const zDna = zombie.dna
+      this.setState({ data: "Your zombie is named " + zName + ", has a dna of " + zDna + " and is of level " + zLevel });
+    }
+
+    levelUp() {
+      const zId = this.state.zombieId;
+      contractMethods.levelUp(this.state.contract, zId, this.state.accounts[0], async () => {
+        await this.showZombie();
+      });
+
     }
 
     render() {
@@ -35,7 +83,7 @@ class Simon extends Component {
             <div className="segment" id="simon">
                 <h1>Simon</h1>
                 <p id="data">{this.state.data}</p>
-                <button id="button" onClick={() => { this.callFunction() }}></button>
+                <button id="button" onClick={() => { this.levelUp() }}>Level Up!</button>
             </div>
         );
     }
