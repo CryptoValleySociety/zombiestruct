eace446d108688d4e519e1c1bcd7d3b71b56e624
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-14 19:39:51 -0700

    ADD functions, kitty contract

--- src/utils/calls/component.js
+++ src/utils/calls/component.js
@@ -5,14 +5,14 @@ import ZombieAttackAbi from '../../../truffle/build/contracts/ZombieAttack.json'
 const initialize = async() => {
   const accounts = await web3.default.eth.getAccounts()
   var obj = {
-<<<<<<< HEAD
     contract: new web3.default.eth.Contract(ZombieAttackAbi.abi, contracts.upperApp),
-=======
-    contract: new web3.default.eth.Contract(ZombieAttackAbi.abi, '0x55ebf5e0d1bd3821f25882705dfb38da582427d8'),
->>>>>>> b84e2a1... ADD update front end once zombie is made, connect to kitty
     accounts: accounts
   }
   return obj;
+}
+
+const getZombiesByOwner = async (contract, from) => {
+    return await contract.methods.getZombiesByOwner(from).call()
 }
 
 const createRandomZombie =  async (contract, name, from, gas) => {
@@ -28,6 +28,39 @@ const attack = async (contract, from, gas, _zombieId, _toId) => {
     .call({ from: from, gas: gas }, (err, res) => {
         return res
     })
+
+const viewTransactionReciept = async(functionCall) => {
+  return await web3.default.eth.getTransactionReceipt(functionCall)
+}
+
+const getZombiesByOwner = async(contract, from) => {
+  return await contract.methods.getZombiesByOwner(from).call()
+}
+
+const createRandomZombie = async(contract, name, from, gas) => {
+  await contract.methods.createRandomZombie(name).send({from: from, gas: gas}).on("receipt", async(receipt) => {
+    return receipt
+  })
+}
+
+const attack = async(contract, from, gas, _zombieId, _toId) => {
+  return await contract.methods.attack(_zombieId, _toId).call({
+    from: from,
+    gas: gas
+  }, (err, res) => {
+    return res
+  })
+}
+
+const connectToKitty = async(contract, from, gas) => {
+  const kittyContract = '0x06012c8cf97BEaD5deAe237070F9587f8E7A266d'
+  await contract.methods.setKittyContractAddress(kittyContract).send({from: from, gas: gas}).on("receipt", async(receipt) => {
+    return receipt
+  })
+}
+
+const feedOnKitty = async(contract, from, gas) => {
+  await contract.methods.feedOnKitty(0, 0).send({from: from, gas: gas})
 }
 
 const viewTransactionReciept = async(functionCall) => {
