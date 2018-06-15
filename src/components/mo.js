b72fa4efb66865a0ebe9124cc270b3eaded4f0b2
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-14 17:10:26 -0700

    SOLVED merge conflicts

--- src/components/mo.js
+++ src/components/mo.js
@@ -66,6 +66,60 @@ class Mo extends Component {
     );
   }
 
+<<<<<<< HEAD
+=======
+    async listen() {
+      const { contract } = this.state
+      console.log(contract);
+      const { _address } = contract
+      console.log(contract);
+      const createZombieEvent = contract.at(_address)
+    }
+
+    async createContract() {
+      const MainContract = new web3.default.eth.Contract(ZombieAttack.abi, '0x1a812d086af307e9796da84a1a1a66dd56bd7443')
+      const accounts = await web3.default.eth.getAccounts()
+      this.setState({
+        contract: MainContract,
+        account: accounts[0]
+      })
+      // await this.listen()
+    }
+
+    async listen() {
+      const { contract } = this.state
+      console.log(contract);
+      const { _address } = contract
+      console.log(contract);
+      const createZombieEvent = contract.at(_address)
+    }
+
+    async createZombie(gas) {
+        const { contract, account } = this.state
+        console.log(account);
+        await contract.methods.createRandomZombie('Mohammad').send({from: account, gas: gas})
+        const zombieOneId = await contract.methods.getZombiesByOwner(account).call()
+    }
+
+  async callFunction() {
+        console.log('hello')
+        await this.createZombie();
+         // call function
+        // wait for reciept then call retrieve data function
+        this.createZombie();
+
+    }
+
+    render() {
+        return (
+            <div className="segment" id="mo">
+                <h1>Mo</h1>
+                <p id="data">{this.state.data}</p>
+                <button id="button" onClick={() => { this.callFunction() }}></button>
+            </div>
+        );
+    }
+>>>>>>> 0fc6985... SOLVED merge conflicts
 }
 
 export default Mo
