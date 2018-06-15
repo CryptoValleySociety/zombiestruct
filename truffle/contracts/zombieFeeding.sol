5503087e038dec5f2e464dccbe6e48ff6518c941
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-14 19:39:51 -0700

    ADD functions, kitty contract

--- truffle/contracts/zombieFeeding.sol
+++ truffle/contracts/zombieFeeding.sol
@@ -49,7 +49,6 @@ contract ZombieFeeding is ZombieFactory {
     _triggerCooldown(myZombie);
   }
 
-
   function feedOnKitty(uint _zombieId, uint _kittyDna) public {
     feedAndMultiply(_zombieId, _kittyDna, "kitty");
   }
