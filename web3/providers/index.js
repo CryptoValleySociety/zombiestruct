c6e9e9a6b54ca2dee73bbf2948584562e7b84291
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-14 17:06:44 -0700

    SOLVE merge conflicts with alexcomponent branch

--- web3/providers/index.js
+++ web3/providers/index.js
@@ -11,34 +11,3 @@ if(test) {
 }
 
 export default web3
-<<<<<<< HEAD
-
-// if (typeof web3 !== 'undefined') {
-//     // Use Mist/MetaMask's provider
-//     console.log('Web3 exist!')
-//     console.log(web3)
-//     web3 = new Web3(web3.currentProvider);
-
-//     web3.version.getNetwork((err, netId) => {
-//         switch (netId) {
-//             case "1":
-//                 console.log('This is mainnet')
-//                 break
-//             case "2":
-//                 console.log('This is the deprecated Morden test network.')
-//                 break
-//             case "3":
-//                 console.log('This is the ropsten test network.')
-//                 break
-//             default:
-//                 console.log('This is an unknown network.')
-//         }
-//     })
-
-// } else {
-//     console.log('No web3? You should consider trying MetaMask!')
-//     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-//     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-// }
-=======
->>>>>>> ADD Smart Contract instantiated function
