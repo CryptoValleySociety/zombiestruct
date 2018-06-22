a57204eb31bd765dd8fea18ba8aee0a231da027d
Alessandro Latif <alessandro@latif-khan.com>
2018-06-22 16:18:12 +0200

    Edit code standards

--- src/components/alex.js
+++ src/components/alex.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import funcLib from '../utils/calls/component';
+import contractMethods from '../utils/calls/component';
 
 import '../App.css'
 
@@ -22,26 +22,27 @@ class Alex extends Component {
     }
 
     async initialize() {
-        const initialObj = await funcLib.initialize()
+        const initialObj = await contractMethods.initialize()
+        const { contract, accounts } = initialObj
         await this.setState({
-            contract: initialObj.contract,
-            accounts: initialObj.accounts
+            contract: contract,
+            accounts: accounts
         })
         this.setUp()
     }
 
     async _getZombie(account) {
-        return await funcLib.getZombiesByOwner(this.state.contract, account)
+        return await contractMethods.getZombiesByOwner(this.state.contract, account)
     }
 
     async _attack(from, gas, _zombieId, _toId) {
-        return await funcLib.attack(this.state.contract, from, gas, _zombieId, _toId)
+        return await contractMethods.attack(this.state.contract, from, gas, _zombieId, _toId)
     }
 
     async createZombie(from, name) {
         const zombies = await this._getZombie(from)
         if(zombies.length === 0){
-            await funcLib.createRandomZombie(this.state.contract, name, from, 300000)
+            await contractMethods.createRandomZombie(this.state.contract, name, from, 300000)
             const arr = await this._getZombie(from)
             return arr[0]
         } else {
@@ -56,7 +57,7 @@ class Alex extends Component {
     }
 
     async attack() {
-        const res = await funcLib.attack(this.state.contract, this.state.accounts[0], 300000, this.state.zombie_one, this.state.zombie_two)
+        const res = await contractMethods.attack(this.state.contract, this.state.accounts[0], 300000, this.state.zombie_one, this.state.zombie_two)
         await this.setState({ winLoss: res})
     }
 
