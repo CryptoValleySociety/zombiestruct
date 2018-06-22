728acb903a800b35df88fe4b157535c428bc1362
Alessandro Latif <alessandro@latif-khan.com>
2018-06-22 16:31:20 +0200

    Add obj unpack on initialize

--- src/components/alex.js
+++ src/components/alex.js
@@ -17,10 +17,6 @@ class Alex extends Component {
         }
     }
 
-    async _attack(from, gas, _zombieId, _toId) {
-        return await contractMethods.attack(this.state.contract, from, gas, _zombieId, _toId)
-
-    }
 
     async attack() {
         const res = await contractMethods.attack(this.state.contract, this.state.accounts[0], 300000, this.state.zombie_one, this.state.zombie_two)
