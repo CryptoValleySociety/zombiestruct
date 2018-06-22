ec239d2d82f89b260738d6f20eb8f242998ceafc
Alessandro Latif <alessandro@latif-khan.com>
2018-06-22 15:45:58 +0200

    Component call changes

--- src/utils/web3/addresses/contracts.js
+++ src/utils/web3/addresses/contracts.js
@@ -1,14 +1,20 @@
+<<<<<<< HEAD
 var fs = require('fs')
 
 
 var contracts = {
     upperApp: '0xc0512d02dce6cb6cedccf9841e885bdc19a887a8',
+=======
+var contracts = {
+    upperApp: '0x4584cc71df7842fc696f723bb1c648e94ee5f039',
+>>>>>>> Component call changes
 }
 
 var updateContractAddresses = (address) => {
     contracts.upperApp = address;
 }
 
+<<<<<<< HEAD
 var storeContract = (address) => {
   const contractObj = {contract: address}
   fs.writeFile("contract.json", JSON.stringify(contractObj), function(err) {
@@ -30,3 +36,9 @@ module.exports = {
     storeContract: storeContract,
     getContractAddress: getContractAddress
 }
+=======
+module.exports = {
+    contracts: contracts,
+    updateContracts: updateContractAddresses
+}
+>>>>>>> Component call changes
