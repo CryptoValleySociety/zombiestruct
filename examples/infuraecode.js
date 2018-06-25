ce9c8beb68ff4a5fe6e21b1e3547ee32e9ce53dd
Alessandro Latif <alessandro@latif-khan.com>
2018-06-25 09:44:01 +0200

    Add examples for infura

---
+++ examples/infuraecode.js
@@ -0,0 +1,34 @@
+const Web3 = require('web3');
+const abi = require('./abi');
+
+const web3 = new Web3();
+
+const contract = new web3.eth.Contract(abi.abi);
+
+function depositFunds() {
+  return contract.methods.depositFunds().encodeABI();
+}
+
+function reclaimFunds() {
+  return contract.methods.reclaimFunds().encodeABI();
+}
+
+function useFunds(user, amount) {
+  return contract.methods.useFunds(user, amount).encodeABI();
+}
+
+function refund(user) {
+  return contract.methods.refund(user).encodeABI();
+}
+
+function showFundsOf(user) {
+  return contract.methods.showFundsOf(user).encodeABI();
+}
+
+module.exports = {
+  depositFunds: depositFunds,
+  reclaimFunds: reclaimFunds,
+  useFunds: useFunds,
+  refund: refund,
+  showFundsOf: showFundsOf
+};
