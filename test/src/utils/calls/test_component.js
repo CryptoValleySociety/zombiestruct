7134e5792efbac086858c85e5df5bed287a7a446
simonmogli <simon@scheynet.de>
2018-06-28 09:58:53 +0200

    Add contractMethods tests 8/8

--- test/src/utils/calls/test_component.js
+++ test/src/utils/calls/test_component.js
@@ -23,7 +23,6 @@ describe('Contract Method Library For Components', () => {
             const res = await contractMethods.createRandomZombie(contract, 'Banter', accounts[0], gas)
             assert.equal(res.events.NewZombie.event, 'NewZombie')
             const resp = await contractMethods.createRandomZombie(contract, 'NotBanter', accounts[1], gas)
-
             assert.equal(resp.events.NewZombie.event, 'NewZombie')
         } catch (err) {
             assert.strictEqual(err.name, 'Error', 'Create double zombie Error not being thrown');
@@ -34,6 +33,7 @@ describe('Contract Method Library For Components', () => {
         const res = await contractMethods.getZombieById(contract, 0);
         assert.equal(res.name, 'Banter');
         const resp = await contractMethods.getZombieById(contract, 1);
+
         assert.equal(resp.name, 'NotBanter');
     });
 
