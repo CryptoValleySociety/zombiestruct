fa20bf53e086e0117086f7d1fe28d9d60a017300
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-06-28 00:40:31 -0700

    ADD automated contract updating, working on connecting to main net

--- src/utils/web3/addresses/contracts.js
+++ src/utils/web3/addresses/contracts.js
@@ -1,12 +1,32 @@
+var fs = require('fs')
+
+
 var contracts = {
-    upperApp: '0x4584cc71df7842fc696f723bb1c648e94ee5f039',
+    upperApp: '0xc0512d02dce6cb6cedccf9841e885bdc19a887a8',
 }
 
 var updateContractAddresses = (address) => {
     contracts.upperApp = address;
 }
 
+var storeContract = (address) => {
+  const contractObj = {contract: address}
+  fs.writeFile("contract.json", JSON.stringify(contractObj), function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+  });
+}
+
+var getContractAddress = () => {
+  const { contract } = require('../../../../truffle/contract.json')
+  return contract
+}
+
 module.exports = {
     contracts: contracts,
-    updateContracts: updateContractAddresses
+    updateContracts: updateContractAddresses,
+    storeContract: storeContract,
+    getContractAddress: getContractAddress
 }
